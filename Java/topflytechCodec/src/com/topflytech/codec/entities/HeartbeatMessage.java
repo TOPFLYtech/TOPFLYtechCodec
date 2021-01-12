2a3dbad1c727da640c899fd231a4c4e58db3462a
TOPFLYtech <yunfengliu@topflytech.com>
2021-01-12 18:21:59 +0800

    update

--- Java/topflytechCodec/src/com/topflytech/codec/entities/HeartbeatMessage.java
+++
@@ -1,9 +0,0 @@
-package com.topflytech.codec.entities;
-
-/**
- * The type Heartbeat message.Protocol number is 25 25 03.
- * Older devices like 8806,8803Pro,You need to respond to the message to the device, otherwise the device will reconnect to the server
- * The new device, like the 8806 plus, needs to be based on the device configuration to decide whether or not to respond to the message
- */
-public class HeartbeatMessage  extends Message{
-}
